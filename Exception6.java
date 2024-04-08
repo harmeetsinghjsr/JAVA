99eec15f911365475577b98f41b895eaa12c8712
Harmeet <hs978698@gmail.com>
2024-04-08 10:37:43 +0530

    javaaaaaaaa

---
+++ Exception6.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Exception6 {
+    public static void main(String[] args) throws IOException{
+        try {
+            FileWriter f=new FileWriter("abcd.txt",true);
+            String content="Hello World";
+            f.write(content);
+            System.out.println("File Written Successfully");
+            f.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+    }
+    FileReader reader=new FileReader("abcd.txt");
+    int ch=0;
+    while((ch=reader.read())!=-1) {
+        System.out.print((char)ch);
+    }
+    reader.close();
+    }
+
+}
