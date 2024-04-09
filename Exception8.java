335ce36f9da4f0624a61f57e3f76f8fe76577b50
Harmeet <hs978698@gmail.com>
2024-04-09 10:23:46 +0530

    niii

---
+++ Exception8.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Exception8 {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream f=new FileOutputStream("abcd.txt");
+            String content="Hello World";
+            // f.write(content.getBytes());
+            byte arr[]=content.getBytes();
+            f.write(arr);
+            f.close();
+            System.out.println("File Written Successfully");
+            FileInputStream file=new FileInputStream("abcd.txt");
+            int ch=file.read();
+            while(ch!=-1) {
+                System.out.print((char)ch);
+                ch=file.read();
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
