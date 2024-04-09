121c5d584bc9506b0071bbb2fc26f224c4536b75
Harmeet <hs978698@gmail.com>
2024-04-09 13:49:27 +0530

    huiii

---
+++ over.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+class over
+{
+void series()
+{
+int i;
+System.out.println("Function 2 ");
+for(i=1;i<=19;i+=2)
+{
+System.out.print(i+" ");
+}
+}
+
+void series(int n)
+{
+int i,a=1;
+System.out.println("Function 1 ");
+for(i=1;i<=n;i++)
+{
+System.out.print(a+" ");
+a+=3;
+}
+}
+
+public static void main(String a[])
+{
+Scanner in=new Scanner(System.in);
+over obj=new over();
+int n;
+System.out.println("Enter n ");
+n=in.nextInt();
+obj.series(n);
+
+obj.series();
+}
+}
