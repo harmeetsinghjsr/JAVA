0e3b19d889c29781b3b00e7e3199eb67a7533e8a
Harmeet <hs978698@gmail.com>
2024-05-05 01:27:19 +0530

    lambda

--- lambda.java
+++ lambda.java
@@ -1,12 +1,10 @@
-public class LambdaExample {
+public class lambda{
     public static void main(String[] args) {
-        // Using lambda function to print numbers from 1 to 10
         Runnable printNumbers = () -> {
             for (int i = 1; i <= 10; i++) {
                 System.out.println(i);
             }
         };
-        // Running the lambda function
         printNumbers.run();
     }
 }
