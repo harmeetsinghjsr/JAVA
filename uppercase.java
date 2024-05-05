f697639953d8f45a038aba6cccca038ebacfb5b6
Harmeet <hs978698@gmail.com>
2024-05-05 09:44:52 +0530

    uppercase

---
+++ uppercase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class uppercase {
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        s=s.toUpperCase();
+        System.out.println(s);
+    }
+}
