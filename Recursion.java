761a326adc6a8e1515bd62444054356a393fe22e
Harmeet <hs978698@gmail.com>
2024-06-08 20:24:38 +0530

    recursion in java

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Recursion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number to find factorial: ");
+        int n = in.nextInt();
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+        sc.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
